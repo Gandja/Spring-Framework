96caee98d950001c0e1c0c15fb8a88abc7695259
Denis Gandzha <gandja.denis@yandex.ru>
2020-03-13 23:33:55 +0300

    Add ConsumingRestApplication.java and ConsumingRestApplication and Value.java

--- spring/src/main/java/com/gandzha/consumingrest/Value.java
+++ spring/src/main/java/com/gandzha/consumingrest/Value.java
@@ -1,5 +1,37 @@
 package com.gandzha.consumingrest;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Value {
 
+    private long id;
+    private String quote;
+
+    public Value() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    @Override
+    public String toString() {
+        return "Value{" +
+                "id=" + id +
+                ", quote='" + quote + '\'' +
+                '}';
+    }
 }
